31444941248c09b0041fd780a2142a19c58ae92f
ryan <ryanef39@gmail.com>
2026-01-16 11:12:30 -0500

    pw

--- src/App.tsx
+++ src/App.tsx
@@ -16,22 +16,7 @@ export default function App() {
       <h1>Auth Example</h1>
 
       <p>
-        This example demonstrates a simple login flow with three pages: a public
-        page, a protected page, and a login page. In order to see the protected
-        page, you must first login. Pretty standard stuff.
-      </p>
 
-      <p>
-        First, visit the public page. Then, visit the protected page. You're not
-        yet logged in, so you are redirected to the login page. After you login,
-        you are redirected back to the protected page.
-      </p>
-
-      <p>
-        Notice the URL change each time. If you click the back button at this
-        point, would you expect to go back to the login page? No! You're already
-        logged in. Try it out, and you'll see you go back to the page you
-        visited just *before* logging in, the public page.
       </p>
 
       <Routes>
@@ -132,10 +117,7 @@ function RequireAuth({ children }: { children: JSX.Element }) {
   let location = useLocation();
 
   if (!auth.user) {
-    // Redirect them to the /login page, but save the current location they were
-    // trying to go to when they were redirected. This allows us to send them
-    // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
+
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
@@ -154,14 +136,10 @@ function LoginPage() {
 
     let formData = new FormData(event.currentTarget);
     let username = formData.get("username") as string;
+    let password = formData.get("password") as string;
 
     auth.signin(username, () => {
-      // Send them back to the page they tried to visit when they were
-      // redirected to the login page. Use { replace: true } so we don't create
-      // another entry in the history stack for the login page.  This means that
-      // when they get to the protected page and click the back button, they
-      // won't end up back on the login page, which is also really nice for the
-      // user experience.
+
       navigate(from, { replace: true });
     });
   }
@@ -173,6 +151,9 @@ function LoginPage() {
       <form onSubmit={handleSubmit}>
         <label>
           Username: <input name="username" type="text" />
+        </label>{" "}
+        <label>
+          Password: <input name="password" type="password" />
         </label>{" "}
         <button type="submit">Login</button>
       </form>
